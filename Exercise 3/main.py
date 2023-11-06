9cf4497b154f68f54b88a4e65452ac0e3922e270
Maxim <roqueknight123456@gmail.com>
2023-11-06 14:24:33 +0300

    Exersice 1-3

--- Exercise 3/main.py
+++ Exercise 3/main.py
@@ -1,21 +1,15 @@
 import os
 
-# Список имен файлов (указать имена всех файлов без расширения)
-file_names = ['1', '2', '3']  # Пример: 1.txt, 2.txt, 3.txt
-
-# Словарь для хранения количества строк в каждом файле
+file_names = ['1', '2', '3']
 file_line_counts = {}
 
-# Получение количества строк в каждом файле и сохранение в словаре
 for name in file_names:
     with open(f"{name}.txt", "r", encoding="utf-8") as f:
         lines = f.readlines()
         file_line_counts[f"{name}.txt"] = len(lines)
 
-# Сортировка файлов по количеству строк
 sorted_files = sorted(file_line_counts.items(), key=lambda x: x[1])
 
-# Запись содержимого файлов в результирующий файл с предварительной служебной информацией
 with open("result.txt", "w", encoding="utf-8") as result_file:
     for file_name, line_count in sorted_files:
         result_file.write(f"{file_name}\n{line_count}\n")
